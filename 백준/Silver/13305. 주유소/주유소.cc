ddbd2111d795dabddcae87e5bc378dc48f9fafb0
geonoda <113406097+geonoda@users.noreply.github.com>
2024-03-06 13:44:46 +0900

    [Silver III] Title: 주유소, Time: 96 ms, Memory: 9832 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -17,16 +17,21 @@ int main(void)
 		cin>>arr2[i];
 	}
 	
-	int min=0;
-	int sum=0;
-	int load=0;
+	long long int min=0;
+	long long int sum=0;
+	long long int load=0;
 	min=arr2[0];
 	load=arr[0];
+	
 	for(int i=0; i<a-1; i++)
 	{
-		if(min<arr2[i+1])
+		if(min<arr2[i+1]||i==a-2)
 		{
 			load=load+arr[i+1];
+			if(i==a-2)
+			{
+				sum=sum+min*load;
+			}
 		
 		}
 		else
