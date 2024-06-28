a2efacfab93e0bdc4f2e4fd302eae231e24d7d88
geonoda <113406097+geonoda@users.noreply.github.com>
2024-06-28 16:08:39 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 36 ms, Memory: 3192 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+priority_queue <int> Q;
+
+int arr[100001];
+
+int main(void)
+{
+	int a=0;
+	cin>>a;
+	
+	for(int i=0; i<a; i++)
+	{
+		int num=0;
+		cin>>num;
+		Q.push(-num);
+	}
+	
+	int sum=0;
+	
+	while(Q.size()>1)
+	{
+		int num1=Q.top();
+		Q.pop();
+		int num2=Q.top();
+		Q.pop();
+		Q.push((num1+num2));
+		sum=sum-(num1+num2);
+	}
+	
+	cout<<sum;
+	return 0;
+}
