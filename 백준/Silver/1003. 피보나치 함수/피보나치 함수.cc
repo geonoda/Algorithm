b1e512206628067ef7d7bc321bc8e613469028f3
geonoda <113406097+geonoda@users.noreply.github.com>
2024-07-04 16:38:56 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -4,8 +4,6 @@ using namespace std;
 
 int DP[41][2];
 
-int fibonacci(int n);
-
 int main(void)
 {
 	int a=0;
@@ -29,19 +27,5 @@ int main(void)
 		cout<<DP[num][0]<<" "<<DP[num][1]<<endl;
 	}
 	
-	fibonacci(5);
 	return 0;
-}
-
-int fibonacci(int n)
-{
-    if (n == 0) {
-        //printf("0");
-        return 0;
-    } else if (n == 1) {
-        //printf("1");
-        return 1;
-    } else {
-        return fibonacci(n-1) + fibonacci(n-2);
-    }
 }
