97bf16bb00b454313aabeabd237a7522806ab7fd
geonoda <113406097+geonoda@users.noreply.github.com>
2024-07-07 19:47:09 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[1001];
+int DP[1001];
+
+int main(void)
+{
+	int a=0;
+	
+	cin>>a;
+	
+	for(int i=1; i<=a; i++)
+	{
+		cin>>arr[i];
+	}
+	
+	for(int i=1; i<=a; i++)
+	{
+		for(int j=1; j<=a; j++)
+		{
+			if(i-j>=0)
+			{
+				if(DP[i]<arr[j]+DP[i-j])
+				{
+					DP[i]=arr[j]+DP[i-j];
+				}
+				
+			}
+		}
+	}
+	
+	cout<<DP[a];
+	
+	return 0;
+}
