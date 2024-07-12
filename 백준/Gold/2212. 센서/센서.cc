a1c6ce97c4a1a707b631f7871ee5c4158bde717f
geonoda <113406097+geonoda@users.noreply.github.com>
2024-07-12 18:02:15 +0900

    [Gold V] Title: 센서, Time: 4 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Gold/2212. 센서/센서.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[10001];
+int gap[10001];
+
+int main(void)
+{
+	int N=0;
+	int K=0;
+	cin>>N>>K;
+	
+	for(int i=0; i<N; i++)
+	{
+		cin>>arr[i];
+	}
+	
+	sort(arr,arr+N);
+	
+	int sum=0;
+	
+	for(int i=0; i<N-1; i++)
+	{
+		gap[i]=arr[i+1]-arr[i];
+		sum+=gap[i];
+	}
+	
+	sort(gap, gap+(N-1));
+	
+	
+	for(int i=0; i<K-1; i++)
+	{
+		if(N-2-i>=0)
+		{
+			sum-=gap[N-2-i];
+		}
+		
+	}
+	
+	cout<<sum;
+	
+	return 0;
+}
