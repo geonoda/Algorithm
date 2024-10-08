6862443ca027a394c847dd1dacb4f9968482a826
geonoda <113406097+geonoda@users.noreply.github.com>
2024-10-08 23:27:11 +0900

    [Gold V] Title: 치킨 배달, Time: 20 ms, Memory: 2052 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int arr[51][51];
+vector<pair<int,int> > V;
+int N=0;
+int M=0;
+int result=987654321;
+void DFS(int count, int pre);
+int main(void)
+{
+	
+	cin>>N>>M;
+	
+	for(int i=1; i<=N; i++)
+	{
+		for(int j=1; j<=N; j++)
+		{
+			cin>>arr[i][j];
+			if(arr[i][j]==2)
+			{
+				V.push_back({i,j});	
+				arr[i][j]=0; 
+			}
+		}
+	}
+	
+	DFS(0,-1);
+	
+	cout<<result;
+	return 0;
+}
+
+int visited[50];
+
+void DFS(int count, int pre)
+{
+	if(count==M)//백터에서 치킨집을 꺼내서 거리를 찾는다. 
+	{
+		int arr2[51][51]={0};
+		
+		for(int i=1; i<=N; i++)
+		{
+			for(int j=1; j<=N; j++)
+			{
+				arr2[i][j]=arr[i][j];
+			}
+		}
+		
+		for(int i=0; i<50; i++)
+		{
+			if(visited[i]==1)
+			{
+				arr2[V[i].first][V[i].second]=2;
+			}
+		}
+		int distance=0;
+		
+		for(int i=1; i<=N; i++)
+		{
+			for(int j=1; j<=N; j++)
+			{
+				if(arr2[i][j]==1)
+				{
+					int temp=987654321;
+					for(int k=0; k<50; k++)
+					{
+						if(visited[k]==1)
+						{
+							//cout<<V[k].first<<" "<<V[k].second<<endl;
+							int x=i-V[k].first;
+							int y=j-V[k].second;
+							if(x<0)
+							{
+								x=(-x);
+							}
+							if(y<0)
+							{
+								y=(-y);
+							}
+							if(temp>x+y)
+							{
+								temp=x+y;
+							}
+						}
+					}
+					distance+=temp;
+				}
+			}
+		}
+		if(distance<result)
+		{
+			result=distance;
+		}
+		
+	}
+	for(int i=0; i<V.size(); i++)
+	{
+		if(visited[i]==0&&i>pre)
+		{
+			visited[i]=1;
+			DFS(count+1,i);
+			visited[i]=0;
+		}	
+	}
+}
