6acdb873888168ebdf99aa2b0db10ee81d708853
geonoda <113406097+geonoda@users.noreply.github.com>
2025-01-14 17:24:23 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 660 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -1,71 +1,72 @@
 #include <iostream>
 #include <queue>
 #include <vector>
-
 using namespace std;
 
 int visited[1000001];
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int T = 0;
+	cin >> T;
+	for (int j = 0; j < T; j++)
+	{
+		int k = 0;
+		cin >> k;
 
-    int T;
-    cin >> T;
+		priority_queue<pair<int,int>> pq1;
+		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq2;
+		fill(visited, visited + 1000001, 0); // visited 초기화
+		for (int i = 0; i < k; i++)
+		{
+			char op;
+			int num;
+			cin >> op >> num;
 
-    while (T--) {
-        int k;
-        cin >> k;
+			if (op == 'I') { // 삽입 연산
+				pq1.push({ num, i });
+				pq2.push({ num, i });
+				visited[i] = 1; // 삽입된 상태로 표시
+			}
+			else if (op == 'D') { // 삭제 연산
+				if (num == 1) { // 최댓값 삭제
+					while (!pq1.empty() && !visited[pq1.top().second]) {
+						pq1.pop(); // 이미 삭제된 값 정리
+					}
+					if (!pq1.empty()) {
+						visited[pq1.top().second] = 0; // 최댓값 삭제
+						pq1.pop();
+					}
+				}
+				else if (num == -1) { // 최솟값 삭제
+					while (!pq2.empty() && !visited[pq2.top().second]) {
+						pq2.pop(); // 이미 삭제된 값 정리
+					}
+					if (!pq2.empty()) {
+						visited[pq2.top().second] = 0; // 최솟값 삭제
+						pq2.pop();
+					}
+				}
+			}
+			
+		}
+		// 최종 출력 전에 큐 정리
+		while (!pq1.empty() && !visited[pq1.top().second]) {
+			pq1.pop();
+		}
+		while (!pq2.empty() && !visited[pq2.top().second]) {
+			pq2.pop();
+		}
 
-        priority_queue<pair<int, int>> max_pq; // 최댓값 우선순위 큐
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> min_pq; // 최솟값 우선순위 큐
-        fill(visited, visited + 1000001, 0); // visited 초기화
-
-        for (int i = 0; i < k; i++) {
-            char op;
-            int num;
-            cin >> op >> num;
-
-            if (op == 'I') { // 삽입 연산
-                max_pq.push({num, i});
-                min_pq.push({num, i});
-                visited[i] = 1; // 삽입된 상태로 표시
-            } else if (op == 'D') { // 삭제 연산
-                if (num == 1) { // 최댓값 삭제
-                    while (!max_pq.empty() && !visited[max_pq.top().second]) {
-                        max_pq.pop(); // 이미 삭제된 값 정리
-                    }
-                    if (!max_pq.empty()) {
-                        visited[max_pq.top().second] = 0; // 최댓값 삭제
-                        max_pq.pop();
-                    }
-                } else if (num == -1) { // 최솟값 삭제
-                    while (!min_pq.empty() && !visited[min_pq.top().second]) {
-                        min_pq.pop(); // 이미 삭제된 값 정리
-                    }
-                    if (!min_pq.empty()) {
-                        visited[min_pq.top().second] = 0; // 최솟값 삭제
-                        min_pq.pop();
-                    }
-                }
-            }
-        }
-
-        // 최종 출력 전에 큐 정리
-        while (!max_pq.empty() && !visited[max_pq.top().second]) {
-            max_pq.pop();
-        }
-        while (!min_pq.empty() && !visited[min_pq.top().second]) {
-            min_pq.pop();
-        }
-
-        // 결과 출력
-        if (max_pq.empty() || min_pq.empty()) {
-            cout << "EMPTY\n";
-        } else {
-            cout << max_pq.top().first << " " << min_pq.top().first << "\n";
-        }
-    }
-
-    return 0;
-}
+		// 결과 출력
+		if (pq1.empty() || pq2.empty()) {
+			cout << "EMPTY\n";
+		}
+		else {
+			cout << pq1.top().first << " " << pq2.top().first << "\n";
+		}
+	}
+	return 0;
+} //100 -100 -200  앞에서 빼고 뺴고 뒤에서 -50이 들어간 후 뺴고 뺴면? -100이 사라져있어야하는데 왜 있지?
