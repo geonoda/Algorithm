449e5e802b16ce41ca31f048f79aa4ff9a1e0506
geonoda <113406097+geonoda@users.noreply.github.com>
2026-09-03 18:31:32 +0900

    [level 2] Title: 올바른 괄호, Time: 0.36 ms, Memory: 4.48 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,36 @@
+#include<string>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    
+    int count = 0;
+    
+    for(int i=0; i<s.size(); i++)
+    {
+        if(s[i] == '(')
+        {
+            count++;
+        }
+        else if(s[i] == ')')
+        {
+            count --;
+        }
+        
+        if(count < 0)
+        {
+            answer = false;
+            break;
+        }
+    }
+    
+    if(count > 0)
+    {
+        answer = false;
+    }
+
+    return answer;
+}
