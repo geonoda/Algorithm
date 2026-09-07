c874e554cdd37704bed4de5b265a8a3d9426df1c
geonoda <113406097+geonoda@users.noreply.github.com>
2026-09-07 18:52:12 +0900

    [level 2] Title: 짝지어 제거하기, Time: 4.39 ms, Memory: 6.57 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<string>
+#include <stack>
+
+using namespace std;
+
+int solution(string s)
+{
+    int answer = -1;
+    stack<char> st;
+    
+    for(int i = 0; i < s.size(); i++)
+    {
+        if(!st.empty() && st.top() == s[i])
+        {
+            st.pop();
+        }
+        else
+        {
+            st.push(s[i]);
+        }
+    }
+    
+    if(st.size() == 0)
+    {
+        answer = 1;
+    }
+    else
+    {
+        answer = 0;
+    }
+
+    return answer;
+}
